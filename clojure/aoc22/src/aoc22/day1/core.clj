6801bc5df60a300070b5cb5abe109a1ce25c3841
Mark Engelberg <mark.engelberg@gmail.com>
2022-12-01 02:35:13 -0800

    Comprehension

--- clojure/aoc22/src/aoc22/day1/core.clj
+++ clojure/aoc22/src/aoc22/day1/core.clj
@@ -13,23 +13,18 @@
             [clojure.data.priority-map :as pm]
             [com.rpl.specter :refer :all]))
 
-(def input (as-> (slurp "src/aoc22/day1/input.txt") %
-             (str/split % #"\n\n")
-             (map str/split-lines %)
-             (map #(map read-string %) %)))
+(def input (for [block (str/split (slurp "src/aoc22/day1/input.txt") #"\n\n")]
+             (map read-string (str/split-lines block))))
 
 ;; ((8462 6981 3714 4409 8186 3614 2218 7558 6702)
 ;;  (2947 4727 6396 5718 2361 1970 1583 2816 2995 6914 4313 1401)
 ;;  (1643 7815 2162 8841 7671)
 ;;  ...)
 
-(def total-calories-per-elf (map #(reduce + %) input))
+(def total-calories-per-elf (map #(apply + %) input))
 
 (def part1-solution (apply max total-calories-per-elf))
 
-(def part2-solution (->> total-calories-per-elf
-                         (sort >)
-                         (take 3)
-                         (apply +)))
+(def part2-solution (->> total-calories-per-elf (sort >) (take 3) (apply +)))
 
 
