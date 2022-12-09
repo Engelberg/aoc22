e8032256fda4c0d0199d70bf713b6d20851a3a4a
Mark Engelberg <mark.engelberg@gmail.com>
2022-12-09 02:37:21 -0800

    Day 9

---
+++ clojure/aoc22/src/aoc22/day9/core.clj
@@ -0,0 +1,48 @@
+(ns aoc22.day9.core
+  (:refer-clojure :exclude [cond if-let if-some when-let when-some])
+  (:require [better-cond.core :refer [cond if-let if-some when-let when-some defnc defnc-]]
+            [clojure.math.numeric-tower :as nt]
+            [clojure.math :as math]
+            [medley.core :as med]
+            [clojure.math.combinatorics :as comb]
+            [clojure.set :as set]
+            [clojure.string :as str]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as alg]
+            [instaparse.core :as insta]
+            [clojure.data.priority-map :as pm]
+            [com.rpl.specter :refer :all]))
+
+(def input (for [l (str/split-lines (slurp "src/aoc22/day9/input.txt"))
+                 :let [[dir n] (str/split l #"\s")]]
+             [(keyword dir) (parse-long n)]))
+
+(defnc v+ [[a b] [c d]] [(+ a c) (+ b d)])
+(def dv {:R [1 0], :L [-1 0], :U [0 -1], :D [0 1]})
+(defnc neighbor? [[x1 y1] [x2 y2]] (and (<= (abs (- x1 x2)) 1) (<= (abs (- y1 y2)) 1)))
+
+(defnc move-tail [[Hx Hy :as head] [Tx Ty :as tail]]
+  (neighbor? head tail) tail
+  (= Hx Tx) (if (< Hy Ty) [Tx (dec Ty)] [Tx (inc Ty)])
+  (= Hy Ty) (if (< Hx Tx) [(dec Tx) Ty] [(inc Tx) Ty])
+  (and (< Hx Tx) (< Hy Ty)) [(dec Tx) (dec Ty)]
+  (and (< Hx Tx) (> Hy Ty)) [(dec Tx) (inc Ty)]
+  (and (> Hx Tx) (< Hy Ty)) [(inc Tx) (dec Ty)]
+  :else [(inc Tx) (inc Ty)])
+
+(defnc simulate-step [rope dir]
+  :let [[Hx Hy :as head] (v+ (rope 0) (dv dir))]
+  (loop [new-rope [head], rope (next rope)]
+    (cond
+      (nil? rope) new-rope 
+      :let [next-knot (move-tail (peek new-rope) (first rope))]
+      (recur (conj new-rope next-knot) (next rope)))))
+
+(defnc unroll-directions [directions] (mapcat (fn [[dir n]] (repeat n dir)) directions))
+
+(defnc count-tail-positions [num-links]
+  (count (into #{} (map peek) (reductions simulate-step (vec (for [i (range num-links)] [0 0]))
+                                          (unroll-directions input)))))
+
+(println (count-tail-positions 2))
+(println (count-tail-positions 10))
