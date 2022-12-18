c8fba74e46f17829fe45646de10d771e0610034f
Mark Engelberg <mark.engelberg@gmail.com>
2022-12-18 01:20:02 -0800

    clojure/aoc22/src/aoc22/day18

--- clojure/aoc22/project.clj
+++ clojure/aoc22/project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [engelberg/data.union-find "1.0.0"]
                  [tarantella "1.1.1"]
                  [engelberg/data.union-find "1.0.0"]
                  [org.clojure/math.numeric-tower "0.0.5"]
