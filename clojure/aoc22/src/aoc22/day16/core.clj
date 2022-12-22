d767717b4eba72f0cef91cec4a84c97ede215f67
Mark Engelberg <mark.engelberg@gmail.com>
2022-12-21 16:01:04 -0800

    Fixed bug

--- clojure/aoc22/src/aoc22/day16/core.clj
+++ clojure/aoc22/src/aoc22/day16/core.clj
@@ -68,16 +68,17 @@
 
 ;; Part 2
 
-(defnc all-completed-states [states completed-states]
+(defnc all-completed-states [states completed-states best-state]
   (= (count states) 0) (vals (apply merge-with (partial max-key :flow) completed-states))
   :let [state (peek states), states (pop states)]
-  (complete? state) (recur states (conj completed-states {(:valves state) state}))
-  (< (upper-bound state) (:flow @best-state)) (recur states completed-states)
-  (recur (into states (next-states state)) completed-states))
+  (complete? state) (recur states (conj completed-states {(:valves state) state})
+                           (if (> (:flow state) (:flow best-state)) state best-state))
+  (< (upper-bound state) (:flow best-state)) (recur states completed-states best-state)
+  (recur (into states (next-states state)) completed-states best-state))
 
-(defnc solution-part-2 "Prints solution quickly, proves optimality in about 4 seconds" []
+(defnc solution-part-2 []
   (binding [*MAXTIME* 26]
-    (loop [human-states (seq (sort-by :flow > (all-completed-states [initial-state] []))),
+    (loop [human-states (seq (sort-by :flow > (all-completed-states [initial-state] [] initial-state))),
            best-elephant-state {:flow 0}]
       (cond
         (nil? human-states) (:flow best-elephant-state)
